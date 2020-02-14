2be024c6706226da95705c287377a9db68c1bde5
Suvodeep Das <suvodeep4119@gmail.com>
2020-02-14 11:08:25 +0530

    ADDED: Feature to add valid first name

--- userRegistrationSystem.sh
+++ userRegistrationSystem.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -x
+#Author: suvodeep4119
 echo "Welcome to REGEX USER REGISTRATION SYSTEM"
+
+#Enable extended globbing
+shopt -s extglob
+
+#Our input
+read -p "Enter first name: " userInput
+#To match
+regex="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $userInput =~ $regex ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
+
